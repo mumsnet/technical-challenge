4cb4920850e1b16f88c378cbe95ac50c66fc3afd
Matthew Davis <matt@mattdavis.co.uk>
2024-10-04 22:24:45 +0100

    Add stub API routes and controllers.

---
+++ app/Http/Controllers/Api/GetCollectorSummaryController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+final class GetCollectorSummaryController
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request): Response
+    {
+        return new Response('TODO: Implement this endpoint.');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\CreateBookController;
+use App\Http\Controllers\Api\GetBookController;
+use App\Http\Controllers\Api\GetCollectorSummaryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('books', CreateBookController::class);
+Route::get('books/{uuid}', GetBookController::class);
+Route::get('collectors/{id}/recently-added', GetCollectorSummaryController::class);
