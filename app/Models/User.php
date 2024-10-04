c1e08da1b1c7a5bbf2f686b0d0fa4c63dcc92ce3
Matthew Davis <matt@mattdavis.co.uk>
2024-10-04 21:03:53 +0100

    Use Laravel safety mechanisms.

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,17 +15,6 @@ final class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,81 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 final class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureDates();
+        $this->configureModels();
+        $this->configureMorphMap();
+        $this->configurePasswordValidation();
+    }
+
+    /**
+     * Configure the application's commands
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands($this->app->isProduction());
+    }
+
+    /**
+     * Configure the dates.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure the models.
+     */
+    private function configureModels(): void
+    {
+        Model::unguard();
+        Model::preventSilentlyDiscardingAttributes();
+        Model::preventAccessingMissingAttributes();
+
+        // Prevent lazy loading always...
+        Model::preventLazyLoading();
+
+        // ...but in production, log the violation instead of throwing an exception
+        if ($this->app->isProduction()) {
+            Model::handleLazyLoadingViolationUsing(function (Model $model, string $relation) {
+                $class = get_class($model);
+
+                info("Attempted to lazy load [{$relation}] on model [{$class}].");
+            });
+        }
+    }
+
+    /**
+     * Configure the application's morph map.
+     */
+    private function configureMorphMap(): void
+    {
+        Relation::enforceMorphMap([
+            'user' => \App\Models\User::class,
+        ]);
+    }
+
+    /**
+     * Configure the password validation rules.
+     */
+    private function configurePasswordValidation(): void
+    {
+        Password::defaults(fn () => app()->isProduction() ? Password::min(8)->uncompromised() : null);
     }
 }
